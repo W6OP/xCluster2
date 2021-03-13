bbaf7eab8672c06442632bf476e2f23047c6383e
Peter Bourget <pbourget@w6op.com>
2021-03-13 11:09:13 -0800

    Initial Commit

---
+++ xCluster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xCluster
+//
+//  Created by Peter Bourget on 3/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xCluster/xClusterApp.swift
@@ -0,0 +1,17 @@
+//
+//  xClusterApp.swift
+//  xCluster
+//
+//  Created by Peter Bourget on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct xClusterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
